a533a78b799ec01663b9cea9406e71d7dad1605c
punit10 <punitcs0067@gmail.com>
2018-04-19 19:39:48 +0530

    change database connection

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,4 @@
 <?php
 $con = mysqli_connect("localhost", "excelarf_punit", "java@123", "excelarf_punit")
 or die('Database Not Connected. Please Fix the Issue! ' . mysql_error());
-$con = mysqli_connect("localhost", "root", "java@123", "punit")
-or die('Database Not Connected. Please Fix the Issue! ' . mysql_error());
 ?>
